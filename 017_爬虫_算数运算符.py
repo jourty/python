c3de82c7f3a9715dfb7fb9df442ab73ad67d41d2
jourty <94302566+jourty@users.noreply.github.com>
2022-04-26 11:34:17 +0800

    Add files via upload

---
+++ 017_爬虫_算数运算符.py
@@ -0,0 +1,32 @@
+a=2
+b=3
+
+print(a+b)
+
+print(a-b)
+
+print(a*b)
+
+print(a/b)
+
+# 取除
+print(a//b)
+#取余
+print(a%b)
+
+# 指数
+print(a**b)
+
+# 扩展  字符串的加法是进行拼接的
+a='123'
+b='456'
+print(a+b)
+
+# 在Python中两端都是字符串才可以进行加法
+# a='123'
+# b=345
+# print(a+b)
+
+# 字符串的乘法是重复输出字符串
+a='rwedc'
+print(a*3)
---
+++ 018_爬虫_赋值运算.py
@@ -0,0 +1,12 @@
+a=10
+print(a)
+
+b=c=20
+print(b)
+print(c)
+
+# 多个变量赋值时要加逗号
+d,e,f=1,2,3
+print(d)
+print(e)
+print(f)
